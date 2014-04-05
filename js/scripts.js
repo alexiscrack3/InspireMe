f73b5744d8da61d957ba6d799b26a802c4a74240
Alexis Ortega <alexis.oc3@gmail.com>
2014-04-05 16:00:38 -0600

    Some ideas for arrow

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,30 @@ $(document).ready(function() {
         $('html, body').animate({ scrollTop: 0 }, 'slow');
         return false;
     });
+    // $('.extra-box').click(function(event) {
+    //     $('nav ul').animate({
+    //         scrollLeft: 0 },
+    //         'slow', function() {
+    //     });
+    // });
+
+    var positionX = $('nav ul').position().left;
+
+    // $('.extra-box').hover(function(e) {
+    //     positionX += 1;
+    //     $('nav ul').animate({
+    //         scrollLeft: positionX },
+    //         'slow', function() {
+    //             console.log(positionX);
+    //     });
+    // }, function(e) {
+    //     console.log('afuera');
+    // });
+
+    $('.extra-box').hover(function(e) {
+        console.log('adentro');
+        $('.extra-box').hover();
+    }, function(e) {
+        console.log('afuera');
+    });
 });
