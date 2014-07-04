816d86784e26e9cd07446b447b9c7aa628a7d1f2
Alexis Ortega <alexis.oc3@gmail.com>
2014-07-03 23:29:18 -0600

    New node server

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var morgan = require('morgan');
+var methodOverride = require('method-override');
+var router = express.Router();
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+app.use(morgan('dev'));
+app.use(methodOverride());
+
+// Routes
+router.get('/', function(req, res) {
+    res.sendfile('./public/index.html');
+});
+
+var server = app.listen(3000, function() {
+    console.log('Listening on port %d', server.address().port);
+});
