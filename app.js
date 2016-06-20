5dd85896f7d8a68dc65e2c7ab485af9b0f00f910
Alexis Ortega <alexis.oc3@gmail.com>
2016-06-19 19:36:29 -0600

    sendfile function is deprecated, using sendFile instead

--- app.js
+++ app.js
@@ -11,27 +11,27 @@ app.use(morgan('dev'));
 // Routes
 var router = express.Router();
 router.get('/', function(req, res) {
-    res.sendfile('./public/index.html');
+    res.sendFile(__dirname + '/public/index.html');
 });
 
 router.get('/favorite', function(req, res) {
-    res.sendfile('./public/favorite.html');
+    res.sendFile(__dirname + '/public/favorite.html');
 });
 
 router.get('/about', function(req, res) {
-    res.sendfile('./public/about.html');
+    res.sendFile(__dirname + '/public/about.html');
 });
 
 app.use('/', router);
 
 app.use(function(req, res, next) {
     res.status(404);
-    res.sendfile('./public/404.html');
+    res.sendFile(__dirname + '/public/404.html');
 });
 
 app.use(function(err, req, res, next) {
     console.log(err.stack);
-    res.status(500).sendfile('./public/500.html');
+    res.status(500).sendFile(__dirname + '/public/500.html');
 });
 
 var server = app.listen(config.port, function() {
